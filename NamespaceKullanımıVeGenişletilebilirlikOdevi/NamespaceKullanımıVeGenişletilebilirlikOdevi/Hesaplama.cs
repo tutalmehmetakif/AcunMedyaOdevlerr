e82f4e4d3ec0b1cef9e9e4b3b038182e4f75231c
Mehmet Akif Tutal <tutalmehmetakif@gmail.com>
2025-03-06 16:04:20 +0300

    Ödev Tamamlandı

---
+++ NamespaceKullanımıVeGenişletilebilirlikOdevi/NamespaceKullanımıVeGenişletilebilirlikOdevi/Hesaplama.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamespaceKullanımıVeGenişletilebilirlikOdevi
+{
+    /* 
+  Namespace’ler, C# projelerinde kod organizasyonunu
+ve sınıfların isim çakışmalarını önlemek için kullanılan önemli bir kavramdır. 
+Namespace’ler, kodunuzu mantıklı gruplar halinde düzenlemenize ve farklı sınıf ve tipleri ayırt etmenize yardımcı olur. 
+
+ Namespace, C# projelerinde kodunuzu düzenlemek ve gruplamak için kullanılan bir yapıdır. 
+  Namespace’ler, kodunuzun hangi modülde veya parçada olduğunu belirtir ve çakışan sınıf isimlerini çözer. 
+
+
+ Genişletilebilirlik, bir yazılımın temel yapısını değiştirmeden 
+yeni özellikler veya işlevler eklemeye olanak tanıyan bir yazılım geliştirme prensibidir.
+Genişletilebilir bir yazılım, değişikliklere kolayca adapte olabilir ve yeni gereksinimlere uyum sağlayabilir.
+
+ Genişletilebilirliği sağlamak için modüler tasarım,
+açık/kapalı prensibi (OCP), bağımlılıkların azaltılması (loose coupling), 
+arayüzler ve soyutlama (interfaces & abstraction) ve plugin/API kullanımı gibi yöntemler uygulanabilir. 
+Yazılımı bağımsız bileşenler halinde geliştirerek yeni bileşenlerin eklenmesini kolaylaştırabilir, 
+mevcut kodu değiştirmeden yeni özellikler eklemek için modüler bir yapı oluşturabiliriz.
+ 
+ */
+    namespace AcunMedyaOdev
+    {
+        public class Hesaplama
+        {
+            public int Topla(int sayi1, int sayi2)
+            {
+                return sayi1+sayi2;
+            }
+
+            public int Carp(int sayi1, int sayi2)
+            {
+               return sayi1* sayi2;
+
+            }
+
+
+        }
+
+
+
+
+
+
+
+    }
+
+}
---
+++ NamespaceKullanımıVeGenişletilebilirlikOdevi/NamespaceKullanımıVeGenişletilebilirlikOdevi/Program.cs
@@ -0,0 +1,26 @@
+﻿using NamespaceKullanımıVeGenişletilebilirlikOdevi.AcunMedyaOdev;
+
+namespace Test
+{
+  class Program
+    {
+        static void Main()
+        {
+            Hesaplama hesaplama = new Hesaplama();
+            int toplamSonucu = hesaplama.Topla(5, 9);
+           int carpimSonucu =  hesaplama.Carp(9, 214);
+
+            Console.WriteLine($"Toplama Sonucu: {toplamSonucu}");
+            Console.WriteLine($"Çarpma Sonucu: {carpimSonucu}");
+
+
+        }
+    }
+
+
+
+
+
+
+
+}
