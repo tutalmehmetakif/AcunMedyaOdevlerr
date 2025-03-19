bc5545790ef51385b933d23b4bfeeace6fbf097c
Mehmet Akif Tutal <tutalmehmetakif@gmail.com>
2025-03-19 13:03:53 +0300

    Ödev Tamamlandı 19.03.2025

---
+++ BasitHesapMakinesi/BasitHesapMakinesi/Matematik.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasitHesapMakinesi
+{
+    public class Matematik
+    {
+        
+        public void hesap(char işlem, double a, double b)
+        {
+            switch(işlem)
+            {
+              
+                case '+':
+                    Console.WriteLine($"Toplama Sonucu = {a+b}");
+                    break;
+
+                case '-':
+                    Console.WriteLine($"Çıkarma Sonucu = {a-b}");
+                    break ;
+
+                case '*':
+                        Console.WriteLine($"Çarpma Sonucu = {a*b}");
+                    break;
+
+                case '/':
+                    Console.WriteLine($"Bölme Sonucu = {a/b}");
+                    break;
+            }
+        }
+
+
+    }
+}
---
+++ BasitHesapMakinesi/BasitHesapMakinesi/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using BasitHesapMakinesi;
+
+Console.WriteLine("Lütfen ilk sayıyı giriniz ");
+double sayı1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Lütfen ikinci sayıyı giriniz");
+double sayı2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Lütfen yapmak istediğiniz işlemi giriniz (+,-,*,/)");
+
+char işlem = Convert.ToChar(Console.ReadLine());
+
+Matematik mat = new Matematik();
+mat.hesap(işlem,sayı1,sayı2);
