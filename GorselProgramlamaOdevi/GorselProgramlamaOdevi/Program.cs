febc666ba2d8966dde6711cbec01364992132f84
Mehmet Akif Tutal <tutalmehmetakif@gmail.com>
2025-03-29 15:41:12 +0300

    Ödev Tamamlandı 29.03.2025

---
+++ GorselProgramlamaOdevi/GorselProgramlamaOdevi/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Görsel Programlama Nedir?
+Görsel programlama, yazılım geliştirme sürecinde kod yazmak yerine
+grafiksel bileşenler (ikonlar, bloklar, düğmeler, bağlantılar) kullanılarak
+programların oluşturulmasını sağlayan bir yaklaşımdır. 
+Bu yöntem, özellikle kullanıcı arayüzü tasarımı, oyun geliştirme, 
+eğitim amaçlı programlama ve otomasyon gibi alanlarda yaygın olarak kullanılır.
+
+Görsel programlama dillerinde (VPL - Visual Programming Languages) 
+sürükle-bırak yöntemiyle bloklar veya nesneler arasındaki ilişkiler belirlenerek 
+bir program akışı oluşturulur. Örnek olarak Scratch, Blockly, MIT App Inventor,
+LabVIEW ve Visual Basic gibi diller verilebilir. */
+
+
+/*Görsel programlama ile metin tabanlı programlama arasındaki en büyük fark, 
+kod yazım şeklidir. Görsel programlamada kullanıcılar, kod yazmak yerine 
+sürükle-bırak yöntemiyle bloklar veya grafiksel öğeler kullanarak 
+program akışını oluştururlar. Bu yöntem, özellikle başlangıç seviyesindeki kullanıcılar 
+için daha kolay ve anlaşılırdır, çünkü yazım hataları yapma olasılığı düşüktür.
+
+Öte yandan, metin tabanlı programlama dillerinde geliştiriciler, 
+manuel olarak kod satırları yazarak işlemleri gerçekleştirir. Bu yaklaşım daha esnektir 
+ve büyük ölçekli, karmaşık projeler geliştirmek için uygundur.
+Ancak, hata yapma olasılığı daha yüksektir ve dilin sözdizimine (syntax) 
+hâkim olmayı gerektirir.
+
+Görsel programlama, genellikle eğitim, oyun geliştirme ve kullanıcı arayüzü tasarımı gibi
+alanlarda tercih edilirken; metin tabanlı programlama, genel yazılım geliştirme, 
+veri analizi, yapay zeka ve sistem programlama gibi daha geniş 
+kullanım alanlarına sahiptir. Görsel programlama, başlangıç için ideal olsa da,
+profesyonel yazılım geliştirme süreçlerinde metin tabanlı programlama daha yaygın olarak
+kullanılmaktadır. */
